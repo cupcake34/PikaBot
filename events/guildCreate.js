76e3f21a92e61546fbd8a98e9bdfa1684f22ea45
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-04 07:10:21 +0530

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,8 +1,7 @@
 const config = require('../config.json');
-const prefix = require('../prefix.json');
 module.exports = guild => {
 client.users.get(config.botownerid).send(`Someone invited me in ${guild.name}, I will do my best there :smiley:`);
 guild.defaultChannel.send({embed: { 
 color:3447003,
-description:`Hello there, I am a Discord.js Bot created by @Aryan 10#7484 for admin, fun, pokemon and misc commands.\nType \`${prefix.admin}help\` to get a full list of commands.\nFor more support, join my discord server, https://discord.gg/W3QCsE6`}});
+description:`Hello there, I am a Discord.js Bot created by @Aryan 10#7484 for admin, fun, pokemon and misc commands.\nType \`${config.prefix}help\` to get a full list of commands.\nFor more support, join my discord server, https://discord.gg/W3QCsE6`}});
 }
