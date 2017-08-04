7486476e88d226896de91e6e939ebe534698b589
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-04 07:36:17 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
-const pikabot = require("../pikabot.json");
+const pikabot = require("../config.json");
 module.exports = client => {
 
 console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
