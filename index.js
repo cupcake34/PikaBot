bffd9fd23a970a2e2f7ee5df359003782cf7bdd0
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-04 07:13:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const fs = require("fs");
 const config = require("./config.json");
 
 require("./util/eventLoader.js")(client);
