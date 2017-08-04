61096d07d5d9834a9564bdef7e389f271a3a91a6
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-04 13:40:42 +0530

    Update banner.js

--- commands/banner.js
+++ commands/banner.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
   const banner = [".png", ".png?style=banner1", ".png?style=banner2", ".png?style=banner3", ".png?style=banner4"];
   var random = Math.floor(Math.random()*banner.length);
   if (message.guild.icon === null) return message.reply("Please set a icon for server before using this command.");
-  message.channel.send({files: [`https://discordapp.com/api/guilds/${message.guild.id}/embed${banner[random]`]});
+  message.channel.send({files: [`https://discordapp.com/api/guilds/${message.guild.id}/embed${banner[random]}`]});
 }
 
 exports.conf = {
