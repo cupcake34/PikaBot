4752a38ce427fbebd165687fd0e37ae86cc18372
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-06 14:07:32 +0530

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,4 +1,4 @@
 const config = require('../config.json')
 module.exports = guild => {
-client.users.get(config.botownerid).send(`Someone kicked/banned me from ${guild.name}, I don't know what I did. Or maybe that server got deleted🤔`);
+client.users.get(config.botOwner).send(`Someone kicked/banned me from ${guild.name}, I don't know what I did. Or maybe that server got deleted🤔`);
 }
