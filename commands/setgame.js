b1a1dcc33eeba7245f7920c08d4e1365d410aabb
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-07 14:36:19 +0530

    Create setgame.js

---
+++ commands/setgame.js
@@ -0,0 +1,28 @@
+exports.run = (client, message, args) => {
+const config = require('../config.json');
+let game = message.content.split(' ').slice(1)
+let playing = game.join(' ')
+if(message.author.id === config.botOwner){
+message.reply("New Game Set");
+client.user.setGame(playing)
+}else {
+message.reply("I only follows my master's command.")
+}
+  
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['setplay'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'setgame',
+  description: 'Sets the bot\'s game.',
+  usage: 'setgame [game]',
+  module: 'Admin',
+  permit: 'Bot Owner Only',
+  alias: '/ setplay'
+};
