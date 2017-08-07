fa702cd7c720f7932e10b110ea59dfa1d80192b8
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-07 14:38:19 +0530

    Create setname.js

---
+++ commands/setname.js
@@ -0,0 +1,24 @@
+exports.run = (client, message, args) => {
+const config = require('../config.json');
+if(message.author.id !== config.botOwner) return message.reply('you are not my owner!!!');
+let doit = message.content.split(" ").slice(1);
+let pikapi = doit.join(" ");
+message.reply('New Name Set!');
+client.user.setUsername(pikapi);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['setun'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'setname',
+  description: 'Sets the name of bot.',
+  usage: 'setname [name]',
+  module: 'Admin',
+  permit: 'Bot Owner Only',
+  alias: '/ setun'
+};
