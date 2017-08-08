c06e4b2a84b6678b4f04336b5ba983bbf5b5cc12
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-08 21:57:35 +0530

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,32 @@
+exports.run = (client, message, args) => {
+const config = require("../config.json");
+const guild = message.guild;
+const embed = new Discord.RichEmbed()
+.setColor(3447003)
+.setAuthor(guild.name, guild.iconURL)
+.setThumbnail(guild.iconURL)
+.addField("Server ID", guild.id)
+.addField("Server Owner", guild.owner.displayName)
+.addField("Members", `${guild.memberCount} Total | ${guild.members.filter(m=>m.user.bot).size} Bots\n${guild.members.filter(m => m.presence.status === 'online').size} Online | ${guild.members.filter(m => m.presence.status === 'idle').size} Idle | ${guild.members.filter(m => m.presence.status === 'dnd').size} Do Not Disturb`")
+.addField("Channels", `${guild.channels.size} Total\n${guild.channels.filter(c=>c.type === "text").size} Text | ${guild.channels.filter(c=>c.type === "voice").size} Voice\n__Default Channel__: **${guild.defaultChannel.name}**`)
+.addField("Roles", `${guild.roles.size} Total\n_Type `${config.prefix}roles` to get a full list of roles._`)
+.addField("Region", guild.region)
+.addField("Created On", guild.createdAt);
+message.channel.send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['sinfo'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'serverinfo',
+  description: 'Shows info on server.',
+  usage: 'serverinfo',
+  module: 'Other',
+  permit: ' ',
+  alias: '/ sinfo'
+};
