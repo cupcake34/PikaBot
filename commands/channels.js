157b0a0e94949f2e1e1dee6116ee7e3c2e8cec73
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-09 15:41:36 +0530

    Update channels.js

--- commands/channels.js
+++ commands/channels.js
@@ -5,7 +5,9 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
     .setTitle("List Of Channels")
     .setColor(3447003)
-    .setDescription(message.guild.channels.map(c=>c.name).join(", "));
+    .setFooter(`Type ${config.prefix}cinfo <channel> to get info on a specific channel.`)
+    .addField("Text", message.guild.channels.filter(c=>c.type === "text").map(c=>c.name).join(", "))
+    .addField("Voice", message.guild.channels.filter(c=>c.type === "voice").map(c=>c.name).join(", "));
   message.channel.send({embed});
 };
 
