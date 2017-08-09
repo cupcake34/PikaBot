ba21003a30fef8bf57316db068ba3de0002df7a8
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-09 15:32:29 +0530

    Create servers.js

---
+++ commands/servers.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  const embed = new Discord.RichEmbed()
+    .setTitle("List Of Servers")
+    .setColor(3447003)
+    .setDescription(client.guilds.map(g=>g.name).join(", "));
+  message.channel.send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['guilds'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'servers',
+  description: 'Shows all the servers the bot is in',
+  usage: 'servers',
+  module: 'Other',
+  permit: ' ',
+  alias: '/ guilds'
+}; 
