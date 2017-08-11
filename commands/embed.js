dedc0fb9631811972a2f355393c668136b621342
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 20:03:40 +0530

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     let msg = message.content.split(" ").slice(1).join(" ");
-    const color = [];
+    const color = [0xF44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4, 0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800, 0xFF5722, 0x795548, 0x9E9E9E, 0x607D8B, 0x000000, 0xFFFFFF]
     const num = Math.floor(Math.random()*color.length);
     const embed = new Discord.RichEmbed()
     .setDescription(msg)
