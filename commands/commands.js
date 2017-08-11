14c7768b7cddcc2ccf310b79125bd381aef13d30
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 12:23:41 +0530

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -4,7 +4,9 @@ const config = require('../config.json');
 exports.run = (client, message, args) => {
   const commandNames = Array.from(client.commands.keys());   
   const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  message.channel.send(`**List Of Commands in ${args[0]}**\`\`\`${client.commands.filter(c => c.help.module === args[0]).map(cmd => `${config.prefix}${cmd.help.name}${' '.repeat(longest - cmd.help.name.length)} :: ${cmd.help.description}`)\`\`\``);
+  message.channel.send(`**List Of Commands in ${args[0]}**\n\`\`\`${client.commands.filter(c => c.help.module === args[0]).map(cmd => {
+    `${config.prefix}${cmd.help.name}${' '.repeat(longest - cmd.help.name.length)} :: ${cmd.help.description}`
+  })\`\`\``);
   message.channel.send({embed: {
     color:4447003,
     description:`Type \`${config.prefix}h <CommandName>\` for help on a spefified command. Eg. \`${config.prefix}h achievement\``
