58de3bf30846065aa68f91a1d9e12ae68a3db03f
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 12:13:37 +0530

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -4,7 +4,7 @@ const config = require('../config.json');
 exports.run = (client, message, args) => {
   const commandNames = Array.from(client.commands.keys());   
   const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
-  message.channel.send(`\`\`\`${client.commands.filter(c => c.help.module === args[0]).map(cmd => `${config.prefix}${cmd.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description})\`\`\`);
+  message.channel.send(`\`\`\`${client.commands.filter(c => c.help.module === args[0]).map(cmd => `${config.prefix}${cmd.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description})\`\`\``);
 };
 
 exports.conf = {
