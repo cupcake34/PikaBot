1a73e96565e7df343de4cd5412bbc9c8859ee03d
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 16:15:32 +0530

    Added punch and slap

---
+++ commands/punch.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, args) => {
+  if (!args[0]) return message.reply("mention someone to use this command.");
+  const bot = `<@${client.user.id}>`;
+  const mention = `<@${message.mention.users.first().id}>`;
+  const author = `<@${message.author.id}>`;
+  if (author === mention) return message.reply("You can't punch yourself.");
+  if (mention === bot) return message.reply("You can't punch me :stuck_out_tongue_winking_eye:");
+  return message.channel.send(`_${author} punched ${mention}!_`);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'punch',
+  description: 'Punches someone for fun.',
+  usage: 'punch [mention]'
+  module: 'Fun',
+  permit: ' ',
+  alias: ' '
+};
---
+++ commands/slap.js
@@ -0,0 +1,25 @@
+exports.run = (client, message, args) => {
+  if (!args[0]) return message.reply("mention someone to use this command.");
+  const bot = `<@${client.user.id}>`;
+  const mention = `<@${message.mention.users.first().id}>`;
+  const author = `<@${message.author.id}>`;
+  if (author === mention) return message.reply("You can't slap yourself.");
+  if (mention === bot) return message.reply("You can't slap me :stuck_out_tongue_winking_eye:");
+  return message.channel.send(`_${author} slapped ${mention}!_`);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'slap',
+  description: 'Slaps someone for fun.',
+  usage: 'slap [mention]'
+  module: 'Fun',
+  permit: ' ',
+  alias: ' '
+};
