91e297aa045f9fc8361a40288c457fe52584c781
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 11:04:52 +0530

    Update motivate.js

--- commands/motivate.js
+++ commands/motivate.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
 
+const color = [0xF44336, 0xE91E63, 0x9C27B0, 0x673AB7, 0x3F51B5, 0x2196F3, 0x03A9F4, 0x00BCD4, 0x009688, 0x4CAF50, 0x8BC34A, 0xCDDC39, 0xFFEB3B, 0xFFC107, 0xFF9800, 0xFF5722, 0x795548, 0x9E9E9E, 0x607D8B, 0x000000, 0xFFFFFF]
+var random = Math.floor((Math.random()*color.length));
 var newmsg = Math.floor((Math.random() * 15) + 1);
 var motivatemsg;
 if (newmsg == 1)
@@ -66,7 +68,7 @@ motivatemsg =`:ballot_box_with_check: Stay positive, WORK HARD and make it happe
 }
 
 const embed = new Discord.RichEmbed()
-.setColor()
+.setColor(color[random])
 .setDescription(motivatemsg);
 message.channel.send({embed}).then(m=>m.react('☑'));
 
