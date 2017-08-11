f91094c0bb128803eabf6743c088aab44a0bf75d
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-11 16:20:39 +0530

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
   if (!args[0]) return message.reply("mention someone to use this command.");
   const bot = `<@${client.user.id}>`;
-  const mention = `<@${message.mention.users.first().id}>`;
+  const mention = `<@${message.mentions.users.first().id}>`;
   const author = `<@${message.author.id}>`;
   if (author === mention) return message.reply("You can't slap yourself.");
   if (mention === bot) return message.reply("You can't slap me :stuck_out_tongue_winking_eye:");
