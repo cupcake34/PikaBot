5fe53675a45f53e8e64d0d4cc4b42b1142c5ebcc
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-12 16:11:19 +0530

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const config = require("../config.json");
+
+exports.run = (client, message, args) => {
+  var mention;
+  if (message.mentions.users.first() !== undefined) {
+    mention = message.mentions.users.first();
+  }else {
+    mention = message.author;
+  };
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00FF00)
+    .addField("Username", mention.tag)
+    .addField("Avatar URL", mention.avatarURL)
+    .setImage(mention.avatarURL);
+  message.channel.send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'avatar',
+  description: 'Shows your someone\' avatar url..',
+  usage: 'avatar [user]',
+  module: 'Fun',
+  permit: ' ',
+  alias: ' '
+}; 
+
