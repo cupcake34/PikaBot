31046bb628c137a02dbf3f574ede2a76479985a7
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-12 12:18:21 +0530

    Update channels.js

--- commands/channels.js
+++ commands/channels.js
@@ -2,12 +2,14 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 
 exports.run = (client, message, args) => {
+  const channelMap = message.guild.channels.map(c=>c.name;   
+  const longest = channelMap.reduce((long, str) => Math.max(long, str.length), 0);
   const embed = new Discord.RichEmbed()
     .setTitle("List Of Channels")
     .setColor(3447003)
     .setFooter(`Type ${config.prefix}cinfo <channel> to get info on a specific channel.`)
-    .addField("Text", message.guild.channels.filter(c=>c.type === "text").map(c=>c.name).join(", "))
-    .addField("Voice", message.guild.channels.filter(c=>c.type === "voice").map(c=>c.name).join(", "));
+    .addField("Text", `\`\`\`${message.guild.channels.filter(c=>c.type === "text").map(c=>${c.name}${' '.repeat(longest - c.name)} :: ${c.id}).join(", ")}\`\`\``)
+    .addField("Voice", `\`\`\`${message.guild.channels.filter(c=>c.type === "voice").map(c=>${c.name}${' '.repeat(longest - c.name)} :: ${c.id}).join(", ")}\`\`\``);
   message.channel.send({embed});
 };
 
