9aa51548f8acee5925b143998d79e78b2c66a074
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-19 06:56:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,6 @@ module.exports = message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return message.reply("Commands are not usable in DM.");
   if (message.channel.type === "group") return;
-  if (message.guild.id === "264445053596991498") return;
-  if (message.guild.id === "110373943822540800") return;
-  if (message.content.toLowerCase().includes("lol")) {
-  message.channel.send("**:joy: :joy: LAUGH OUT LOUD :joy: :joy:**").then(msg => msg.delete(7000));
-  }else
-  if (message.content.toLowerCase().includes("xd" || "rofl" || "lmao" || "rofl" || "lmfao")) {
-  message.channel.send(":joy: :joy: :joy: :joy: :joy: :joy:").then(msg => msg.delete(5000));
-  }else
   if (!message.content.startsWith(settings.prefix)) return;
   let command = message.content.split(' ')[0].slice(settings.prefix.length);
   let args = message.content.split(' ').slice(1);
