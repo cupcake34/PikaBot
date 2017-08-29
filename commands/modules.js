f6a1bf26b6ffe762b5cb497a8d6d484f08357c30
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-29 16:16:21 +0530

    Update modules.js

--- commands/modules.js
+++ commands/modules.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
     .setTitle("List Of Modules")
     .setColor(4447003)
     .setDescription(client.modules.map(m => `• ${m.moduledata.name}`).join("\n"))
-    .setFooter(`:information_source:️Type \`${config.prefix}cmds <ModuleName>\` to get a list of commands in that module.`);
+    .setFooter(`:information_source:️ Type \`${config.prefix}cmds <ModuleName>\` to get a list of commands in that module.`);
   message.channel.send({embed});
 };
 
