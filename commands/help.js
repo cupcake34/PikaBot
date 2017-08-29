332ef5f83662d3da934617c64bb7868c85c877eb
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-29 16:13:47 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const settings = require('../config.json');
+const config = require('../config.json');
 exports.run = (client, message, args) => {
   if (!args[0]) {
     const commandNames = Array.from(client.commands.keys());
