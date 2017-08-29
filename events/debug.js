877fc724b0d73ff7d258187a6dad8f9a4ee8dea2
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-29 07:48:32 +0530

    Update debug.js

--- events/debug.js
+++ events/debug.js
@@ -1,5 +1,3 @@
 module.exports = (client, debugmsg) => {
-  const owner = client.users.get(require("../config.json").botOwner);
-  owner.send(`\`\`\`js\n\n${debugmsg}\`\`\``);
   console.log(debugmsg);
 };
