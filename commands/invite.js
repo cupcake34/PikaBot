cbabb91db90967c703e4c0b06ef4b2b2e7ffaba9
Aryan10 <aryan.shivam.10@gmail.com>
2017-08-31 10:17:13 +0530

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
   const embed = new Discord.RichEmbed()
     .setAuthor(bot.username, bot.avatarURL)
     .setThumbnail(bot.avatarURL)
-    .setDescription(`If you want me in your server, [click here](https://discordapp.com/oauth2/authorize?permissions=2146958591&scope=bot&client_id=${bot.id})!\n\nIf you want to get latest updates join my [Discord Server](https://discord.gg/wTeksG).\n\nI am also on...\n[bots.discord.pw](https://bots.discord.pw/bots/${bot.id})\n[discordbots.org](https://discordbots.org/bot/${bot.id})`);
+    .setDescription(`If you want me in your server, [click here](https://discordapp.com/oauth2/authorize?permissions=2146958591&scope=bot&client_id=${bot.id})!\n\nIf you want to get latest updates join my [Discord Server](${config.serverinvite}).\n\nI am also on...\n[bots.discord.pw](https://bots.discord.pw/bots/${bot.id})\n[discordbots.org](https://discordbots.org/bot/${bot.id})`);
     message.channel.send({embed});
 };
 
