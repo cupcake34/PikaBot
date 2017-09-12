def47d2c84be20baf2ee0bfeb1eefb40b7a5ea17
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-12 11:58:18 +0530

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -3,16 +3,16 @@ exports.run = (client, message, args) => {
   const bot = `<@${client.user.id}>`;
   const mention = `<@${message.mentions.users.first().id}>`;
   const author = `<@${message.author.id}>`;
+  const embed = new Discord.RichEmbed()
+  .setTitle("_${author} hugs ${mention}!_")
+  .setcolor(ff00ff)
+  .setImage("https://media.giphy.com/media/8tpiC1JAYVMFq/giphy.gif")
+  .addBlankField(true);
   
   if (author === mention) return message.reply("Alone",{file: [`https://media.tenor.com/images/f32c20bad2f5dfa59113ac0bfdfc26b1/tenor.gif`]});
  
-  return message.channel.send({
-                         "embed": { 
-                           title: '_${author} slapped ${mention}!_',
-                                   
-         "image" : { "url" : "https://media.giphy.com/media/8tpiC1JAYVMFq/giphy.gif"}
-            }
-        });
+  return  message.channel.send({embed});
+                         
 };
 
 exports.conf = {
