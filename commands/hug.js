9348e8427e47e6db929526b0d5e3e21e699605a8
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-12 10:02:35 +0530

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -3,7 +3,7 @@ exports.run = (client, message, args) => {
   const bot = `<@${client.user.id}>`;
   const mention = `<@${message.mentions.users.first().id}>`;
   const author = `<@${message.author.id}>`;
-  if (author === mention) return message.reply{file: `https://media.tenor.com/images/f32c20bad2f5dfa59113ac0bfdfc26b1/tenor.gif`)};
+  if (author === mention) return message.reply(Alone,{file: [`https://media.tenor.com/images/f32c20bad2f5dfa59113ac0bfdfc26b1/tenor.gif`]});
  
   return message.channel.send({file: `https://media.giphy.com/media/8tpiC1JAYVMFq/giphy.gif`});
 };
