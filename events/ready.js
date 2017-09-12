d2d7659a6f92a36222a4761565b1d0855f70f72f
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-12 21:17:37 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,16 @@
 const pikabot = require("../config.json");
+
 module.exports = client => {
 
 console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
 
-if (pikabot.streaming === true) {
-  client.user.setGame(pikabot.game, pikabot.streamingurl);
-  }else {
-  client.user.setGame(pikabot.game)
-  client.user.setStatus(pikabot.status);										    
-}
+let streaming = true;
+client.user.setStatus(pikabot.status);
+
+if (streaming === true) {
+client.user.setGame(pikabot.game, pikabot.streamingurl);
+} else {
+client.user.setGame(pikabot.game)
+}	
 
 }
