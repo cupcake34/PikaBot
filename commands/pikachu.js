61b3108dd65fa398071413146c3a37286a69e543
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-14 20:44:21 +0530

    Update pikachu.js

--- commands/pikachu.js
+++ commands/pikachu.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
 const pikachu = [
-'file: "https://sp.yimg.com/ib/th?id=OIP.3pnUZMYxkjgvG5dnwEJvIQEsEs&pid=15.1&rs=1&c=1&qlt=95&w=111&h=111"'
-'file: "https://media.giphy.com/media/MJcf161ptwviU/giphy.gif"'
-'file: "https://tse3.mm.bing.net/th?id=OIP.Mxl1Ov6x6rLk0tvgrElhZwEsDb&pid=15.1&P=0&w=228&h=168"'
-'file: "https://tse1.mm.bing.net/th?id=OIP.oSBx19na-Xq2kZnad9Y5GwHgFs&pid=15.1&P=0&w=225&h=172"'
-'file: "https://sp.yimg.com/ib/th?id=OIP.t3OOCktNFRVnfnTxnxSCGQEeDa&pid=15.1&rs=1&c=1&qlt=95&w=145&h=111"'
+   'https://sp.yimg.com/ib/th?id=OIP.3pnUZMYxkjgvG5dnwEJvIQEsEs&pid=15.1&rs=1&c=1&qlt=95&w=111&h=111'
+    'https://media.giphy.com/media/MJcf161ptwviU/giphy.gif'
+  'https://tse3.mm.bing.net/th?id=OIP.Mxl1Ov6x6rLk0tvgrElhZwEsDb&pid=15.1&P=0&w=228&h=168'
+'https://tse1.mm.bing.net/th?id=OIP.oSBx19na-Xq2kZnad9Y5GwHgFs&pid=15.1&P=0&w=225&h=172'
+ 'https://sp.yimg.com/ib/th?id=OIP.t3OOCktNFRVnfnTxnxSCGQEeDa&pid=15.1&rs=1&c=1&qlt=95&w=145&h=111'
 ];
 message.channel.send(pikachu[Math.floor(Math.random() * pikachu.length));
 }
