2fb96a60fa9f7c1c452986879be607d4b2376502
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-18 08:33:56 +0530

    Update setstream.js

--- commands/setstream.js
+++ commands/setstream.js
@@ -1,28 +1,28 @@
-exports.run = (client, message, args) => {
-const config = require('../config.json');
-let game = message.content.split(' ').slice(1)
-let playing = game.join(' ')
-if(message.author.id === config.botOwner){
-message.reply("New Game Set");
-client.user.setGame(playing, 'discordapp.com')
-}else {
-message.reply("I only follows my master's command.")
+exports . run  = ( client , message , args ) => {
+const  config  =  require ( ' ../config.json ' );
+let game =  message . content . split ( '  ' ). slice ( 1 )
+let playing =  game . join ( '  ' )
+if ( Message . author . id  ===  config . botOwner ) {
+message . reply ( " New Game Set " );
+customer . user . setGame (playing, ' discordapp.com ' )
+} else {
+message . reply ( " I only follow my master's command. " )
 }
   
 }
 
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: [],
-  permLevel: 4
+exports . conf  = {
+  enabled :  true ,
+  guildOnly :  false ,
+  aliases : [],
+  permLevel :  4
 };
 
-exports.help = {
-  name: 'setstream',
-  description: 'Sets the streaming game of bot.',
-  usage: 'setstream [game]',
-  module: 'Admin',
-  permit: 'Bot Owner Only',
-  alias: ' '
+exports . help  = {
+  name :  ' setstream ' ,
+  description :  ' Sets the streaming game of bot. ' ,
+  usage :  ' setstream [game] ' ,
+  module :  ' Admin ' ,
+  permit :  ' Bot Owner Only ' ,
+  alias :  '  '
 };
