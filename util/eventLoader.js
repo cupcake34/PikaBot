d0c07ddedd3a0429395e50dcaadb92b452c499ef
Such civic icy civic icy I coach it's is go read ZR ztx <rjtkingxd@gmail.com>
2017-09-23 16:17:56 +0530

    Add files via upload

--- util/eventLoader.js
+++ util/eventLoader.js
@@ -3,15 +3,20 @@ const msgEvent = (event) => require(`../bot.js`)
 module.exports = client => {
   client.on('ready', () => reqEvent('ready')(client));
   client.on('error', () => reqEvent('error')(client));
+  client.on('reconnecting', () => reqEvent('reconnecting')(client));
+  client.on('disconnect', () => reqEvent('disconnect')(client));
   client.on('warn', () => reqEvent('warn')(client));
   client.on('debug', () => reqEvent('debug')(client));
   client.on('roleDelete', reqEvent('roleDelete'));
   client.on('roleCreate', reqEvent('roleCreate'));
+  client.on('guildCreate', reqEvent('guildCreate'));
+  client.on('guildDelete', reqEvent('guildDelete'));
   client.on('guildMemberAdd', reqEvent('guildMemberAdd'));
-
+  client.on('guildMemberRemove', reqEvent('guildMemberRemove'));
   client.on('guildBanAdd', reqEvent('guildBanAdd'));
   client.on('guildBanRemove', reqEvent('guildBanRemove'));
   client.on('channelDelete', reqEvent('channelDelete'));
   client.on('channelCreate', reqEvent('channelCreate'));
+  client.on('channelUpdate', reqEvent('channelUpdate'));
   client.on('message', msgEvent('message'));
 };
